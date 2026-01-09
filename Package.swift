a784291c80d07ce1c4dee58d8dcf6ac6ac24cc40
Emory Al-Imam <emorya@gmail.com>
2026-01-08 16:05:52 -0800

    Release 1.0.5

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
         .library(name: "BindJS", targets: ["BindJS", "BindJSDependencies"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/yapstudios/SVGView.git", from: "1.0.7"),
+        // GLTFKit2 is a binary framework that must be provided separately
+        // (SVGViewKit is statically linked into BindJS.framework, so not needed here)
         .package(url: "https://github.com/warrenm/GLTFKit2", from: "0.5.11")
     ],
     targets: [
         .binaryTarget(
             name: "BindJS",
-            url: "https://github.com/yapstudios/bindjs-apple-binary/releases/download/1.0.4/BindJS.xcframework.zip",
-            checksum: "1f8489c113fbabdecbcfb19b5a12c93db0751c078b630542e25e68bc725e5020"
+            url: "https://github.com/yapstudios/bindjs-apple-binary/releases/download/1.0.5/BindJS.xcframework.zip",
+            checksum: "dc9c134cec40113734f0e1e3295f1281d2ea6704eb6cdd185a4e58816246bdf3"
         ),
         .target(name: "BindJSDependencies", dependencies: [
-            .product(name: "SVGViewKit", package: "SVGView"),
             .product(name: "GLTFKit2", package: "GLTFKit2")
         ]),
     ]
