e628be32729cea5b84eb5c77b4d11b5615df0719
Emory Al-Imam <emory@yapstudios.com>
2026-01-20 20:31:21 -0800

    MET-360: Use explicit tag to workaround SPM resolution error (#2)
    Use explicit tag to workaround SPM resolution error

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // GLTFKit2 is a binary framework that must be provided separately
         // (SVGViewKit is statically linked into BindJS.framework, so not needed here)
-        .package(url: "https://github.com/warrenm/GLTFKit2", revision: "e69e354c4f31ea07b7816b8cbe2ddb28e897d073")
+        .package(url: "https://github.com/warrenm/GLTFKit2", exact: "0.5.14")
     ],
     targets: [
         .binaryTarget(
